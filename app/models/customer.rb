2208ab9612475b4b0a8b99eb55dc1a9fddde6da2
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-09-15 14:21:55 +0000

    [update]ログイン画面　レイアウト調整

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -50,11 +50,11 @@ class Customer < ApplicationRecord
 
    # プロフィール画像呼び出しメソッド
   def get_profile_image(width, height)
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize_to_fill: [width, height]).processed
+   unless profile_image.attached?
+    file_path = Rails.root.join('app/assets/images/profile_image.jpg')
+    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+   end
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 
   def customer_status
