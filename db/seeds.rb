a2fd9a1b569828fe0372247c314b4d02e937788c
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-09-25 20:32:52 +0000

    mainへブランチ切り替え

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,10 @@
 
 # 管理者登録
 Admin.create!(email: "admin@email.com", password: "adminpass")
+
+# 会員登録
+
+# Customer.create!(name: "Yamada Taro", email: "yamada.taro@gmail.com", password: "111111")
+# Customer.create!(name: "Sato Mamoru", email: "sato.mamoru@gmail.com", password: "111111")
+# Customer.create!(name: "Saito Kazuma", email: "saito.kazuma@gmail.com", password: "111111")
+# Customer.create!(name: "Noda Koki", email: "koki.noda@gmail.com", password: "111111")
