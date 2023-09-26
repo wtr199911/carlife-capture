a6c227d9aed75bcb636dc46b4949c81f067a47f1
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-09-26 11:23:59 +0000

    微調整

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,24 +5,24 @@ require 'carrierwave/storage/fog'
 # 保存先の分岐
 CarrierWave.configure do |config|
   # 本番環境はS3に保存
-  if Rails.env.production?
-    config.storage = :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'inspigram-image-store'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/inspigram-image-store'
-    # iam_profile
-    config.fog_credentials = {
-      provider: 'AWS',
-      # credentialsで管理
-      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1' #東京リージョン
-    }
-    # キャッシュをS3に保存
-    # config.cache_storage = :fog
-  else
-    # 開発環境はlocalに保存
+  # if Rails.env.production?
+  #   config.storage = :fog
+  #   config.fog_provider = 'fog/aws'
+  #   config.fog_directory  = 'inspigram-image-store'
+  #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/inspigram-image-store'
+  #   # iam_profile
+  #   config.fog_credentials = {
+  #     provider: 'AWS',
+  #     # credentialsで管理
+  #     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+  #     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+  #     region: 'ap-northeast-1' #東京リージョン
+  #   }
+  #   # キャッシュをS3に保存
+  #   # config.cache_storage = :fog
+  # else
+  #   # 開発環境はlocalに保存
     config.storage :file
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-  end
+  # end
 end
