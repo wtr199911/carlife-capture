708c2ef7d66906e766e5146b18b4dbcd17a77a91
EC2 Default User <ec2-user@ip-172-31-40-61.ap-northeast-1.compute.internal>
2023-09-26 06:33:02 +0000

    [add]seed 記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,50 @@
 # 管理者登録
 Admin.create!(email: "admin@email.com", password: "adminpass")
 
-# 会員登録
+# テストデータ
 
-# Customer.create!(name: "Yamada Taro", email: "yamada.taro@gmail.com", password: "111111")
-# Customer.create!(name: "Sato Mamoru", email: "sato.mamoru@gmail.com", password: "111111")
-# Customer.create!(name: "Saito Kazuma", email: "saito.kazuma@gmail.com", password: "111111")
-# Customer.create!(name: "Noda Koki", email: "koki.noda@gmail.com", password: "111111")
+mountain = Tag.find_or_create_by!(name: "山")
+ocean = Tag.find_or_create_by!(name: "海")
+offroad = Tag.find_or_create_by!(name: "オフロード")
+onroad = Tag.find_or_create_by!(name: "オンロード")
+
+#ユーザー1
+
+koki = Customer.find_or_create_by!(email: "koki@example.com") do |customer|
+    customer.name = "Koki"
+    customer.profile_text = "カーライフを楽しんでます！"
+    customer.password ="password"
+end
+
+#ユーザー2
+
+minato = Customer.find_or_create_by!(email: "minato@example.com") do |customer|
+    customer.name = "Minato"
+    customer.profile_text = "フォトスポットを巡ってます"
+    customer.password ="password"
+end
+
+
+#投稿1
+
+Post.find_or_create_by!(title: "オフロードを最高に楽しむ") do |post|
+    post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample2.jpg"), filename:"sample2.jpg")
+    post.detail = "オフロードを駆け抜けて撮影しました！"
+    post.prefecture_id = 4
+    post.place = "4x4 FIELD 4駆村"
+    post.customer = koki
+    post.tags << mountain
+    post.tags << offroad
+end
+
+#投稿2
+
+Post.find_or_create_by!(title: "バックに四国と瀬戸内海が綺麗に撮れる場所") do |post|
+    post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample1.jpg"), filename:"sample1.jpg")
+    post.detail = "遠出して撮影してきました！"
+    post.prefecture_id = 33
+    post.place = "倉敷市 鷲羽山展望台"
+    post.customer = minato
+    post.tags << ocean
+    post.tags << onroad
+end
